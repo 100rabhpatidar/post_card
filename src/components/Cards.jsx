cdaf9ccf00e5e02875ed4945d474092cc9668deb
100rabhpatidar <sourabhpatidar1920@gmail.com>
2024-07-08 17:01:01 +0530

    last commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts, removePost } from '/src/slicer/postSlice';
 
